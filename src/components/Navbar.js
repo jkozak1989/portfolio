b13567ee70c920f764f34ffaf31d5ae4ab64d0dd
jkozak1989 <98820821+jkozak1989@users.noreply.github.com>
2022-06-13 11:17:01 +0100

    add navbar logo button/meta data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import "./Navbar.css";
 
@@ -7,17 +7,19 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className='navbar-logo'>
-                <svg width="100%" viewBox="0 0 100 100" height="100%" src='logo.svg'>
-                    <g transform="matrix(1,0,0,1,15.5389,9.82986)">
-                        <text x="-0.555px" y="46.667px" style={{'fontFamily':'Courier','fontSize':'66.667px'}}>J</text>
-                    </g>
-                    <g transform="matrix(1,0,0,1,42.7113,9.82986)">
-                        <text x="-0.555px" y="46.667px" style={{'fontFamily':'Courier','fontSize':'66.667px'}}>K</text>
-                    </g>
-                    <g transform="matrix(1,0,0,1,-0.0844655,21.2026)">
-                        <text x="4.244px" y="58.638px" style={{'fontFamily':'Courier','fontSize':'30.556px'}}>codes</text>
-                    </g>
-                </svg>
+                <Link to='/'>
+                    <svg width="100%" viewBox="0 0 100 100" height="100%" src='logo.svg'>
+                        <g transform="matrix(1,0,0,1,15.5389,9.82986)">
+                            <text x="-0.555px" y="46.667px" style={{'fontFamily':'Courier','fontSize':'66.667px'}}>J</text>
+                        </g>
+                        <g transform="matrix(1,0,0,1,42.7113,9.82986)">
+                            <text x="-0.555px" y="46.667px" style={{'fontFamily':'Courier','fontSize':'66.667px'}}>K</text>
+                        </g>
+                        <g transform="matrix(1,0,0,1,-0.0844655,21.2026)">
+                            <text x="4.244px" y="58.638px" style={{'fontFamily':'Courier','fontSize':'30.556px'}}>codes</text>
+                        </g>
+                    </svg>
+                </Link>
             </div>
             <nav>
                 <NavLink to="/" className='nav-item'><i className="bi bi-house-door-fill"></i> Home</NavLink>
