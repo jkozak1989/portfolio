03887cf30cbf87c0206a519f2553ec22d7ff9628
jkozak1989 <98820821+jkozak1989@users.noreply.github.com>
2022-06-15 19:50:59 +0100

    add skills list to portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,26 +8,37 @@ const Portfolio = () => {
         {
             name: 'Pixel Art Maker',
             description: 'This project allows user to create a 32x32 pixel image. Implemented with React.',
+            skills: 'HTML, CSS, Javascript, React',
             link: 'pixel_art_maker',
             github: 'https://github.com/jkozak1989/portfolio/tree/master/src/components/PixelArtMaker'
         },
         {
             name: 'Wheelspin',
             description: 'This project implements a reward wheel as seen in many competitions.',
+            skills: 'HTML, CSS, Javascript, React',
             link: 'wheelspin',
             github: 'https://github.com/jkozak1989/portfolio/tree/master/src/components/Wheelspin'
         },
         {
             name: 'Memory Game',
             description: 'This project implements a fully playable Memory Game. Created with React.',
+            skills: 'HTML, CSS, Javascript, React',
             link: 'memory_game',
             github: 'https://github.com/jkozak1989/portfolio/tree/master/src/components/MemoryGame'
         },
         {
             name: 'Crypto Price Widget',
             description: 'This project connects to coingecko API to get the list of all crypto currencies and checks for their price in USD.',
+            skills: 'HTML, CSS, Javascript, React',
             link: 'crypto_price_widget',
             github: 'https://github.com/jkozak1989/portfolio/tree/master/src/components/CryptoPriceWidget'
+        },
+        {
+            name: 'Colorblind Helper',
+            description: 'Project uses decision tree algorithm to recognise colors in the picture and upon clicking prints them to the console.',
+            skills: 'Python, Machine Learning',
+            link: '',
+            github: 'https://github.com/jkozak1989/python/tree/main/colorblindHelper'
         }
     ]
     return (
@@ -38,8 +49,9 @@ const Portfolio = () => {
                     <div key={project.name} className='project'>
                         <h3>{project.name}</h3>
                         <p>{project.description}</p>
+                        <p style={{'color':'var(--primary)'}}>Skills: {project.skills}</p>
                         <div>
-                            <Link to={project.link}>See project</Link><br/>
+                            {project.link && <Link to={project.link}>See project</Link>}<br/>
                             <a href={project.github}>See source code</a>
                         </div>
                     </div>
